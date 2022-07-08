f615b650022f0d05720946c344a07bb69f89763f
coelhodiana <coelhodiana04@gmail.com>
2022-07-08 19:59:44 -0400

    form estudante

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const botaoLote = document.querySelector('.botao-lote');
 const divEstudante = document.querySelector('.estudante');
 const divLote = document.querySelector('.lote');
 const formInscricaoLote = document.querySelector('.form-inscricao-lote')
+const formInscricaoEstudante = document.querySelector('.form-inscricao-estudante')
 const botaoMailchimp = document.querySelector('#mc-embedded-subscribe')
 const linkPagamentoLote = document.querySelector('.link-pagamento-lote')
 const email = document.querySelector('#mce-EMAIL')
+const botaoEstudante = document.querySelector('.botao-estudante')
 
 botaoLote.addEventListener('click', mostrarFormInscricaoLote )
+botaoEstudante.addEventListener('click', mostrarFormInscricaoEstudante )
 
 botaoMailchimp.addEventListener('click', mostrarLinkPagamento)
 
@@ -22,4 +25,9 @@ function mostrarLinkPagamento() {
         linkPagamentoLote.scrollIntoView();
     }
 
+}
+
+function mostrarFormInscricaoEstudante() {
+    divEstudante.style.display = 'none';
+    formInscricaoEstudante.style.display = 'block'
 }
