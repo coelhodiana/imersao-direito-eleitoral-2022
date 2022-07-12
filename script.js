f03f7bc46ccc1f537954391eff580dbfb7e0d32c
coelhodiana <coelhodiana04@gmail.com>
2022-07-12 19:14:07 -0400

    feat: convidados

--- script.js
+++ script.js
@@ -1,33 +1,68 @@
-const botaoLote = document.querySelector('.botao-lote');
-const divEstudante = document.querySelector('.estudante');
-const divLote = document.querySelector('.lote');
-const formInscricaoLote = document.querySelector('.form-inscricao-lote')
-const formInscricaoEstudante = document.querySelector('.form-inscricao-estudante')
-const botaoMailchimp = document.querySelector('#mc-embedded-subscribe')
-const linkPagamentoLote = document.querySelector('.link-pagamento-lote')
-const email = document.querySelector('#mce-EMAIL')
-const botaoEstudante = document.querySelector('.botao-estudante')
+const botaoLote = document.querySelector(".botao-lote");
+const divEstudante = document.querySelector(".estudante");
+const divLote = document.querySelector(".lote");
+const formInscricaoLote = document.querySelector(".form-inscricao-lote");
+const formInscricaoEstudante = document.querySelector(
+  ".form-inscricao-estudante"
+);
+const botaoMailchimp = document.querySelector("#mc-embedded-subscribe");
+const linkPagamentoLote = document.querySelector(".link-pagamento-lote");
+const email = document.querySelector("#mce-EMAIL");
+const botaoEstudante = document.querySelector(".botao-estudante");
+const botaoConvidadoRob = document.querySelector(
+  ".convidados__mais-info--roberio"
+);
+const botaoConvidadoLeland = document.querySelector(
+  ".convidados__mais-info--leland"
+);
+const botaoConvidadoMaria = document.querySelector(
+  ".convidados__mais-info--maria"
+);
+const cvRoberio = document.querySelector(".convidados__cv-roberio");
+const cvLeland = document.querySelector(".convidados__cv-leland");
+const cvMaria = document.querySelector(".convidados__cv-maria");
 
-botaoLote.addEventListener('click', mostrarFormInscricaoLote )
-botaoEstudante.addEventListener('click', mostrarFormInscricaoEstudante )
+botaoLote.addEventListener("click", mostrarFormInscricaoLote);
+botaoEstudante.addEventListener("click", mostrarFormInscricaoEstudante);
 
-botaoMailchimp.addEventListener('click', mostrarLinkPagamento)
+botaoMailchimp.addEventListener("click", mostrarLinkPagamento);
+
+botaoConvidadoRob.addEventListener("click", maisInformacoes);
+botaoConvidadoLeland.addEventListener("click", maisInformacoesLeland);
+botaoConvidadoMaria.addEventListener("click", maisInformacoesMaria);
 
 function mostrarFormInscricaoLote() {
-    divLote.style.display = 'none';
-    formInscricaoLote.style.display = 'block'
+  divLote.style.display = "none";
+  formInscricaoLote.style.display = "block";
 }
 
 function mostrarLinkPagamento() {
-    if(email.value.length >= 5) {
-        formInscricaoLote.style.display = 'none';
-        linkPagamentoLote.style.display = 'flex';
-        linkPagamentoLote.scrollIntoView();
-    }
-
+  if (email.value.length >= 5) {
+    formInscricaoLote.style.display = "none";
+    linkPagamentoLote.style.display = "flex";
+    linkPagamentoLote.scrollIntoView();
+  }
 }
 
 function mostrarFormInscricaoEstudante() {
-    divEstudante.style.display = 'none';
-    formInscricaoEstudante.style.display = 'block'
+  divEstudante.style.display = "none";
+  formInscricaoEstudante.style.display = "block";
+}
+
+function maisInformacoes() {
+  cvRoberio.style.display === "block"
+    ? (cvRoberio.style.display = "none")
+    : (cvRoberio.style.display = "block");
+}
+
+function maisInformacoesLeland() {
+  cvLeland.style.display === "block"
+    ? (cvLeland.style.display = "none")
+    : (cvLeland.style.display = "block");
+}
+
+function maisInformacoesMaria() {
+  cvMaria.style.display === "block"
+    ? (cvMaria.style.display = "none")
+    : (cvMaria.style.display = "block");
 }
